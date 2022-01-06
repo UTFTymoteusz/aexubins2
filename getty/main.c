695c1f703ead0d5341bc41f46696e43fca4df084
UTFTymoteusz <tymoteusz.paliwoda@gmail.com>
2022-01-06 22:17:25 +0100

    a lot of stuff

--- getty/main.c
+++ getty/main.c
@@ -1,5 +1,7 @@
+#include <errno.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char* argv[]) {
@@ -10,28 +12,53 @@ int main(int argc, char* argv[]) {
 
     char* filename = argv[1];
 
-    FILE* tty_r = fopen(filename, "r");
-    FILE* tty_w = fopen(filename, "w");
+    FILE* tty_0 = fopen(filename, "r");
+    if (!tty_0) {
+        perror("open new stdin");
+        return 1;
+    }
 
-    dup2(fileno(tty_r), 0);
-    dup2(fileno(tty_w), 1);
-    dup2(fileno(tty_w), 2);
+    FILE* tty_1 = fopen(filename, "w");
+    if (!tty_1) {
+        perror("open new stdout");
+        return 1;
+    }
 
-    fclose(tty_r);
-    fclose(tty_w);
+    FILE* tty_2 = fopen(filename, "w");
+    if (!tty_2) {
+        perror("open new stderr");
+        return 1;
+    }
+
+    dup2(fileno(tty_0), 0);
+    dup2(fileno(tty_1), 1);
+    dup2(fileno(tty_2), 2);
 
     printf("it works [%s]\n", filename);
 
+    FILE* bong = fopen("/out.txt", "w");
+    /*if (!bong) {
+        perror("fopen");
+        return 1;
+    }*/
+
     while (true) {
-        char c = getchar();
+        int c = getchar();
         if (c == '\r')
             c = '\n';
 
-        putchar(c);
+        if (c == -1)
+            perror("getchar");
+
+        if (c == EOF)
+            break;
+
+        if (putc(c, bong) == -1) {
+            perror("write");
+            break;
+        }
     }
 
-    while (true)
-        sleep(1);
-
+    fclose(bong);
     return 0;
 }
